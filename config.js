8382a6fd508c035d3443ee2b81fbea895897c4bb
azkahamam <azkahamam0427@gmail.com>
2026-02-05 09:36:06 +0700

    Fix supabase client initialization

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = 'URL_ANDA_DI_SINI';
 const SUPABASE_KEY = 'ANON_KEY_ANDA_DI_SINI';
 
-const supabase = lib.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Fungsi sederhana untuk cek login admin
 function cekLogin() {

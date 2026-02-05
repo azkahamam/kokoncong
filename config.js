e32a12ebc7d1b046e84628a2c2df5def217f82e9
azkahamam <azkahamam0427@gmail.com>
2026-02-05 09:45:55 +0700

    Update Supabase configuration and client variable
    Updated Supabase URL and key, renamed client variable.

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 // GANTI DENGAN DATA DARI SUPABASE ANDA
-const SUPABASE_URL = 'URL_ANDA_DI_SINI';
-const SUPABASE_KEY = 'ANON_KEY_ANDA_DI_SINI';
+const SUPABASE_URL = 'https://azkahamam.github.io/kokoncong/';
+const SUPABASE_KEY = 'https://pnmhnfkgkakvspadorkq.supabase.co';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Gunakan variabel ini di file lain
+window.supabase = supabaseClient;
 
 // Fungsi sederhana untuk cek login admin
 function cekLogin() {
